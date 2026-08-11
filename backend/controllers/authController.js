fc13308b2994f0e0c57d40846f2af1e7f8936aa4
Subodhkumar <snp42u@gmail.com>
2026-08-11 23:51:40 +0530

    feat: build full-stack Metro Operations Dashboard using React, Node.js, Express, MongoDB with JWT auth, train CRUD, analytics, and production-ready config

---
+++ backend/controllers/authController.js
@@ -0,0 +1,54 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const register = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email });
+
+        if (user) {
+            return res.status(400).json({ msg: "User already exists" });
+        }
+
+        const hashed = await bcrypt.hash(password, 10);
+
+        user = new User({ email, password: hashed });
+        await user.save();
+
+        res.json(user);
+    } catch (err) {
+        console.log("REGISTER ERROR:", err);
+        res.status(500).json({ msg: "Server Error" });
+    }
+};
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({ msg: "Invalid credentials" });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({ msg: "Invalid credentials" });
+        }
+
+        const token = jwt.sign(
+            { id: user._id },
+            "secret123",
+            { expiresIn: "1d" }
+        );
+
+        res.json({ token });
+    } catch (err) {
+        console.log("LOGIN ERROR:", err);
+        res.status(500).json({ msg: "Server Error" });
+    }
+};
---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Register from "./pages/Register.jsx";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
