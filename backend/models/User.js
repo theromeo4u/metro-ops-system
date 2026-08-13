ff226f432f2863aca6cce3efb8ad97a0c752eb03
Subodhkumar <snp42u@gmail.com>
2026-08-13 19:12:16 +0530

    refactor: update User model to include name field and adjust role enum order

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,11 +1,27 @@
+// import mongoose from "mongoose";
+
+// const userSchema = new mongoose.Schema({
+//     email: String,
+//     password: String,
+//     role: {
+//         type: String,
+//         enum: ["admin", "user"],
+//         default: "user",
+//     },
+// });
+
+// export default mongoose.model("User", userSchema);
+
+
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
+    name: String,
     email: String,
     password: String,
     role: {
         type: String,
-        enum: ["admin", "user"],
+        enum: ["user", "admin"],
         default: "user",
     },
 });
