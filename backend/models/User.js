89e177049b03cc28004ee865f67b3db5239e3979
Subodhkumar <snp42u@gmail.com>
2026-08-13 17:52:13 +0530

    refactor: remove commented-out code in User model for cleaner implementation

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,20 +1,3 @@
-// import mongoose from "mongoose";
-
-// const userSchema = new mongoose.Schema({
-//     email: {
-//         type: String,
-//         required: true,
-//         unique: true,
-//     },
-//     password: {
-//         type: String,
-//         required: true,
-//     },
-// });
-
-// export default mongoose.model("User", userSchema);
-
-
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
