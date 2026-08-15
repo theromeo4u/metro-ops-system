5b5a80bd5927c0081a602ab6ef1e2878047717e8
Subodhkumar <111661624+theromeo4u@users.noreply.github.com>
2026-08-15 23:25:04 +0530

    Clean up api.js by removing unused code
    Removed commented-out code for API setup.

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,19 +1,3 @@
-// import axios from "axios";
-
-// const API = axios.create({
-//     baseURL: "http://localhost:5001/api"
-// });
-
-// // attach token automatically
-// API.interceptors.request.use((req) => {
-//     const token = localStorage.getItem("token");
-//     if (token) {
-//         req.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return req;
-// });
-
-// export default API;
 
 import axios from "axios";
 
