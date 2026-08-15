4e814199a55af67ab24bff20e82b97922117bb9b
Subodhkumar <111661624+theromeo4u@users.noreply.github.com>
2026-08-15 23:26:04 +0530

    Clean up main.jsx by removing commented code
    Removed commented-out import statements and ReactDOM rendering code.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,4 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import App from "./App.jsx";
-// import "./styles/index.css";
 
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// );
 import "./index.css";
 import "./styles/index.css";
 import React from "react";
