cb7e0f52dd5b6e91074846a29bc0dfb8662f9afa
Subodhkumar <snp42u@gmail.com>
2026-08-16 22:02:30 +0530

    Clean up User.js by removing commented-out code

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,18 +1,3 @@
-// import mongoose from "mongoose";
-
-// const userSchema = new mongoose.Schema({
-//     email: String,
-//     password: String,
-//     role: {
-//         type: String,
-//         enum: ["admin", "user"],
-//         default: "user",
-//     },
-// });
-
-// export default mongoose.model("User", userSchema);
-
-
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
