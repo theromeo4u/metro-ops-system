e7b235c3ac24456aad8a4a6c521910b917a35728
Subodhkumar <snp42u@gmail.com>
2026-08-16 22:03:30 +0530

    Clean up db.js by removing commented-out code

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const connectDB = async () => {
-//     try {
-//         await mongoose.connect(process.env.MONGO_URI);
-//         console.log("MongoDB Connected");
-//     } catch (err) {
-//         console.error(err);
-//         process.exit(1);
-//     }
-// };
-
-// module.exports = connectDB;
-
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
