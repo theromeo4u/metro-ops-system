95f8f716e1671a6cc33bb61608a0bbd4b1a313a9
Subodhkumar <snp42u@gmail.com>
2026-08-16 22:01:37 +0530

    Clean up Navbar.jsx by removing commented-out code

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,29 +5,6 @@
 
 // //   const logout = () => {
 // //     localStorage.removeItem("token");
-// //     navigate("/");
-// //   };
-
-// //   return (
-// //     <div className="flex justify-between items-center bg-white p-4 shadow">
-// //       <h1 className="text-xl font-bold">🚆Mumbai Metro Dashboard</h1>
-// //       <button
-// //         onClick={logout}
-// //         className="bg-red-500 text-white px-4 py-2 rounded"
-// //       >
-// //         Logout
-// //       </button>
-// //     </div>
-// //   );
-// // }
-
-// // import { useNavigate } from "react-router-dom";
-
-// // export default function Navbar() {
-// //   const navigate = useNavigate();
-
-// //   const logout = () => {
-// //     localStorage.removeItem("token");
 // //     localStorage.removeItem("role"); // ✅ ADD THIS
 // //     navigate("/");
 // //   };
